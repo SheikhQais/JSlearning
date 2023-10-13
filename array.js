1c93472da1f76012cfc038ae148016933af175de
SheikhQais <sheikhqais27@gmail.com>
2023-10-13 23:09:38 +0500

    Arrays methods

--- array.js
+++ array.js
@@ -1,17 +1,17 @@
 const Honda = [
-    {
+    faizan= {
         name: "civic",
         make: "2023",
         variant: "Nat",
         engine: "2000cc"
     },
-    {
+    ahmad = {
         name: "Vezel",
         make: "2020",
         variant: "Hyb",
         engine: "1600cc"
     },
-    {
+    ali = {
         name: "Accord",
         make: "2023",
         variant: "Turbo",
@@ -22,6 +22,25 @@ const Honda = [
 // console.log(Honda[1]);
 
 for(i=0; i<Honda.length; i++){
-    console.log(Honda[i]);
+    if (Honda[i]==ahmad) {
+        console.log(Honda[i]);
+    }
+    else{
+        console.log(Honda[i]);
+    }
 }
-console.log(Honda.length)
+Honda.splice(1,2);
+console.log("After Splice, Array is: ");
+console.log(Honda);
+
+for(i=1; i<=10; i++){
+    Honda.push(i);
+}
+console.log("After pushing new element to end of array: ");
+console.log(Honda);
+
+for(i=1; i<=10; i++){
+    Honda.pop();
+}
+console.log("After poping new element to end of array: ");
+console.log(Honda);
