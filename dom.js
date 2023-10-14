b74b7e545be93c1b559e00b0631e2d704c71a506
SheikhQais <sheikhqais27@gmail.com>
2023-10-14 06:29:13 +0500

    learning DOM

---
+++ dom.js
@@ -0,0 +1,22 @@
+// let a=0,b='',c=null;
+// console.log(typeof a ,typeof b ,typeof c);
+
+// function sum( a, b){
+//     return a+b;
+// }
+// console.log(typeof sum);
+
+// const arr= [1,2,3,4,5];
+// const obj = {1:2,2:3,3:4,4:5,5:6};
+
+// console.log(typeof arr);
+
+// console.log(typeof obj);
+
+// console.log(Array.isArray(arr));
+
+var a = document.querySelector(".footer-right");
+// console.log(a);
+// console.log(typeof a);
+a.innerHTML = "Kesa ha rae tuu!!?";
+a.Style.backgroundColor = "pink";
