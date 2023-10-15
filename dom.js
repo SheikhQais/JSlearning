6435edb23ace0f7110d68d26339cf471445777c1
SheikhQais <sheikhqais27@gmail.com>
2023-10-15 19:16:39 +0500

    practicing API

--- dom.js
+++ dom.js
@@ -1,5 +1,5 @@
 var a = document.querySelector("button");
-a.style.backgroundColor = "transparent";
+a.style.backgroundColor = "red";
 let flag = 0;
 a.addEventListener("click",function(){
     if (flag==0) {
@@ -12,3 +12,10 @@ a.addEventListener("click",function(){
         flag =0;
     }
 })
+
+let promise = fetch("https://goweather.herokuapp.com/weather/Ny")
+promise.then((value)=>{
+    return value.json();
+}).then((value2)=>{
+    console.log(value2)
+});
